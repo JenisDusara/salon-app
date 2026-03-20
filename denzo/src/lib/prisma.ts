a648bd461eb8d906aab184b105930a69ae616a67
Akshay Arora <akshay1.py@gmail.com>
2026-03-21 00:09:39 +0530

    Update prisma.ts

--- denzo/src/lib/prisma.ts
+++ denzo/src/lib/prisma.ts
@@ -1,12 +1,14 @@
-import { PrismaClient } from "../generated/prisma/client";
 import { PrismaNeon } from "@prisma/adapter-neon";
+import { PrismaClient } from "../generated/prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
 function createPrismaClient() {
-  const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL! });
+  const adapter = new PrismaNeon({
+    connectionString: process.env.DATABASE_URL!,
+  });
   return new PrismaClient({
     adapter,
     log: process.env.NODE_ENV === "development" ? ["error"] : ["error"],
