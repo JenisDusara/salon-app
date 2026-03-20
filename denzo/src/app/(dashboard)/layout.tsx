d8b348cd3250b109c6e4d0f4b131a6178743f69a
Akshay Arora <akshay1.py@gmail.com>
2026-03-20 23:56:38 +0530

    Create layout.tsx

---
+++ denzo/src/app/(dashboard)/layout.tsx
@@ -0,0 +1,18 @@
+import { Sidebar } from "@/components/layout/Sidebar";
+import { TopBar } from "@/components/layout/TopBar";
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex bg-slate-100 min-h-screen">
+      <Sidebar />
+      <div className="flex flex-col flex-1 min-h-screen" style={{ marginLeft: "240px" }}>
+        <TopBar />
+        <main className="flex-1 p-7 overflow-y-auto">{children}</main>
+      </div>
+    </div>
+  );
+}
